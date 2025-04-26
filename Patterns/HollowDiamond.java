69cea5028f2831fd70aa747472f7974c53a2f63e
Krishna <krishnakumar.x18@gmail.com>
2025-04-26 21:54:27 +0530

    .

--- Patterns/HollowDiamond.java
+++ Patterns/HollowDiamond.java
@@ -1,4 +1,5 @@
-package Java;
+package Patterns;
+
 public class HollowDiamond {
     public static void main(String[] args)
 {
--- Patterns/StarSquare.java
+++ Patterns/StarSquare.java
@@ -1,4 +1,4 @@
-package Java;
+package Patterns;
 public class StarSquare {
     public static void main(String[] args) {
         for(int i=1;i<=5;i++)
